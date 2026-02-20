d352495b724a1a9d4bb57add9e76ecbac648cf5f
AvneeshP30 <avneesh.pandey_cs.aiml24@gla.ac.in>
2026-02-20 11:13:19 +0530

    add documentation for MG995 Closed-Loop Axis Control System

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
-#include <Arduino.h>
-void setup() {
-// write your initialization code here
-}
-
-
-void loop() {
-// write your code here
-}
+/**
+* @file main.ino
+ * @brief MG995 Closed-Loop Axis Control System
+ * @author Avneesh PAndey
+ * @date 2025-02-20
+ *
+ * @details
+ * Implements PWM-based closed-loop control for MG995 servo motor.
+ * Accepts angle input via Serial Monitor and validates range (0–180).
+ */
